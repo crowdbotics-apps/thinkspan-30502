24b31490dec0e58ac14b2f1b795be4db5064dad5
Crowdbotics <team@crowdbotics.com>
2021-09-13 13:48:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "thinkspan_30502Identifier",
+  url: "https://thinkspan_30502Identifier.botics.co",
+  api: "https://thinkspan_30502Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
